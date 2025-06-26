98aa1767b3e0b95f9ad7c3cc29dfe3e48118ace8
Lamin Jaiteh <modoulaminjaiteh16@gmail.com>
2025-06-26 16:29:15 +0000

    updating the toggle button

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-const btn = document.getElementById("btn") as any;
-const textAnswer1 = document.getElementById("text-answer") as HTMLElement;
-const plusIcon = document.getElementById("plus-icon") as HTMLElement;
-const minusIcon = document.getElementById("minus-icon") as HTMLElement;
+const btn = document.querySelectorAll(".btn") as NodeListOf<HTMLButtonElement>;
+const textAnswer = document.querySelectorAll(".text-answer");
+const plusIcon = document.querySelectorAll(".plus-icon");
+const minusIcon = document.querySelectorAll(".minus-icon");
 
 // btn.addEventListener("click", () => {
 //   // if (!textAnswer1 || !plusIcon || !minusIcon) return;
@@ -11,18 +11,16 @@ const minusIcon = document.getElementById("minus-icon") as HTMLElement;
 //   minusIcon.classList.toggle("hidden");
 // });
 
-btn.addEventListener("click", () => {
-  textAnswer1.classList.toggle("hidden");
-  minusIcon.classList.toggle("hidden");
-  plusIcon.classList.toggle("hidden");
-});
-
-// btn.forEach((button: Element) => {
-//   button.addEventListener("click", () => {
-//     textAnswer1.classList.toggle("hidden");
-//     minusIcon.classList.toggle("hidden");
-//     plusIcon.classList.toggle("hidden");
-//   });
-
-//   alert()
+// btn.addEventListener("click", () => {
+//   textAnswer1.classList.toggle("hidden");
+//   minusIcon.classList.toggle("hidden");
+//   plusIcon.classList.toggle("hidden");
 // });
+
+btn.forEach((button: Element, index) => {
+  button.addEventListener("click", () => {
+    textAnswer?.[index].classList.toggle("hidden");
+    minusIcon?.[index].classList.toggle("hidden");
+    plusIcon?.[index].classList.toggle("hidden");
+  });
+});
